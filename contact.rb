b5ea4b18b53b03294dac2b1ac87d4008f77943b4
chiu <vchiucode@gmail.com>
2015-05-05 01:49:14 +0000

    test

--- contact.rb
+++ contact.rb
@@ -1,25 +1,42 @@
 class Contact
- 
-  attr_accessor :name, :email
 
-  def initialize(name, email)
+  attr_accessor :name, :email
+  
+  @@contact_id = 0
+
+  def initialize(contact_id , name, email)
     # TODO: assign local variables to instance variables
+    @contact_id = contact_id
+    @name = name
+    @email = email
+
   end
- 
+
+  public
+
   def to_s
     # TODO: return string representation of Contact
   end
- 
+
   ## Class Methods
   class << self
+    attr_accessor :contact_array
+    @@contact_array = []
+
     def create(name, email)
       # TODO: Will initialize a contact as well as add it to the list of contacts
+      new_contact = self.new(@@contact_id, name, email)
+      @@contact_array << new_contact
+      @@contact_id += 1
+      return new_contact
+
+
     end
- 
+
     def find(index)
       # TODO: Will find and return contact by index
     end
- 
+
     def all
       # TODO: Return the list of contacts, as is
     end
@@ -27,7 +44,12 @@ class Contact
     def show(id)
       # TODO: Show a contact, based on ID
     end
+
+
+    def print_contact_array
+      puts @@contact_array
+    end
     
   end
- 
+
 end
--- contact_list.rb
+++ contact_list.rb
@@ -1,2 +1,44 @@
 require_relative 'contact'
 require_relative 'contact_database'
+
+
+
+
+
+
+print 'enter the keyword help for the help menu: ' 
+
+
+user_input = gets.chomp.downcase
+
+
+case user_input
+
+
+when 'help'
+  puts 'You need help!!!'
+  puts 'Here is a list of available commands:'
+  puts 'new  - Create a new contact'
+  puts 'list - List all contacts'
+  puts  'show - Show a contact'
+  puts 'find - Find a contact'
+
+
+when 'new'
+  puts "Time for some new stuff!"
+  current_contact = Contact.new(77, 'maxwell Smith',  'maxgmail.com')
+  puts current_contact.inspect
+
+  puts trial_contact = Contact.create("adam smith", "adam@gmail.com").inspect
+ #puts trial_contact.print_contact_array
+ #puts current_contact.print_contact_array
+# puts Contact.contact_array.inspect
+  puts Contact.contact_array
+
+else
+  puts 'You just making it up!'
+end
+
+
+#puts current_contact.@@contact_array.inspect
+
