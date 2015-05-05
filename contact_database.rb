1fc41605134fba3a321b6072e504ca4e3a15de5d
chiu <vchiucode@gmail.com>
2015-05-05 03:05:06 +0000

    cool

--- contact_database.rb
+++ contact_database.rb
@@ -1,2 +1,45 @@
 ## TODO: Implement CSV reading/writing
 require 'csv'
+
+
+class ContactDatabase
+
+  class << self
+
+    def open_file
+      fname = "contact_list.txt"
+      somefile = File.open(fname, "w")
+      somefile.puts "Hello file!"
+      somefile.close
+    end
+
+
+    def add_contact(current_contact)
+      fname = "contact_list.txt"
+      somefile = File.open(fname, "w")
+      somefile.puts "contact: #{current_contact.inspect}"
+      somefile.close
+    end
+
+    def csv_add_contact(current_contact)
+
+      CSV.open('our-new-customers-file.csv', 'a') do |csv_object|
+        
+
+        #string_needed = current_contact.inspect
+        csv_object << ['chris', 'chris@gmail.com']
+        csv_object <<  [current_contact.name, current_contact.email]
+        #csv_object << ['44','66']
+        
+      
+      end
+    end
+
+
+  end
+
+
+end
+
+
+
--- contact_list.rb
+++ contact_list.rb
@@ -1,8 +1,8 @@
 require_relative 'contact'
 require_relative 'contact_database'
+require 'pry'
 
-
-
+#include "file_access"
 
 
 
@@ -29,13 +29,14 @@ when 'new'
   current_contact = Contact.new(77, 'maxwell Smith',  'maxgmail.com')
   puts current_contact.inspect
 
-  puts trial_contact = Contact.create("adam smith", "adam@gmail.com").inspect
 
-  puts "test"
-  Contact.print_contact_array
-  puts "test"
+  #puts trial_contact = Contact.create("adam smith", "adam@gmail.com").inspect
 
-  puts Contact.get_contact_array.inspect
+ # puts "test"
+ # Contact.print_contact_array
+ # puts "test"
+
+ # puts Contact.get_contact_array.inspect
 
 else
   puts 'You just making it up!'
@@ -43,4 +44,10 @@ end
 
 
 #puts current_contact.@@contact_array.inspect
+
+
+#ContactDatabase.open_file
+#ContactDatabase.add_contact(current_contact)
+
+ContactDatabase.csv_add_contact(current_contact)
 
